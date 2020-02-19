c0898692ffc079e478302f3ccdb090caa590308d
Jeff Farr <jfarr@augustash.com>
2020-02-18 20:12:27 -0600

    added blog page with pagination

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,23 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-const Image = () => {
+const Image = ({ image, withFallback = false, ...props }) => {
   const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+      query {
+          fallBackImage: file(relativePath: { eq: "fallback.svg" }) {
+              publicURL
           }
-        }
       }
-    }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  /**
+   * Return fallback Image, if no Image is given.
+   */
+  if (!image) {
+    return withFallback ? <img src={data.fallBackImage.publicURL} alt={"Fallback"} {...props}/> : null
+  }
+
+  return <img src={image.sourceUrl} alt={image.altText} {...props}/>
 }
 
 export default Image
